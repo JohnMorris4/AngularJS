51cd03f0cf63905cc919b9c17353e103f7e66637
JohnMorris4 <johnmorris@morrisje.com>
2016-12-08 00:36:18 -0500

    second commit

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,24 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms'
 
 import { AppComponent }  from './app.component';
+import {HttpModule} from '@angular/http';
+
+import  {SearchComponent} from './components/search/search.component';
+import  {NavbarComponent} from './components/navbar/navbar.component';
+import  {AboutComponent} from './components/about/about.component';
+import {ArtistComponent} from './components/artist/artist.component';
+
+import {routing} from './app.routing';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, routing, FormsModule, HttpModule ],
+  declarations: [ AppComponent,
+   				  SearchComponent, 
+   				  NavbarComponent, 
+   				  AboutComponent,
+            ArtistComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
